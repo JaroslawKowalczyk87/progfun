a4f916018171bdc8962f476cfb1dae3c08efdc62
Jaroslaw Kowalczyk <jar.kowalczyk87@gmail.com>
2017-02-13 14:35:09 +0100

    balance checker finished

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -19,7 +19,17 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+
+      def loop(counter: Int, chars: List[Char]): Int = {
+        if (chars.isEmpty) counter else
+        if (counter == 0 && chars.head == 41) 1 else
+        if (chars.head == 40) loop(counter+1,chars.tail) else
+        if (counter>0 && chars.head == 41) loop(counter-1,chars.tail) else loop(counter,chars.tail)
+      }
+
+      if (loop(0,chars).equals(0)) true else false
+    }
   
   /**
    * Exercise 3
--- recfun/src/main/scala/recfun/worksheet.sc
+++ recfun/src/main/scala/recfun/worksheet.sc
@@ -1,7 +1,11 @@
-def pascal(c: Int, r: Int): Int = if (c==0 || r==c) 1
-else pascal(c-1, r-1) +  pascal(c, r-1)
+def balance(chars: List[Char]): Boolean = {
 
-pascal(0,2)
-pascal(1,2)
-pascal(1,3)
-pascal(2,4)
+  def loop(counter: Int, chars: List[Char]): Int = {
+    if (chars.isEmpty) counter else
+    if (counter == 0 && chars.head == 41) 1 else
+    if (chars.head == 40) loop(counter+1,chars.tail) else
+    if (counter>0 && chars.head == 41) loop(counter-1,chars.tail) else loop(counter,chars.tail)
+  }
+
+  if (loop(0,chars).equals(0)) true else false
+}
